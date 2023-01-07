3a0165b3f7cbd12795fdc5c13f4f1704a880f6e5
Matheus Silva <103164463+MSOliver1998@users.noreply.github.com>
2023-01-07 09:50:04 -0300

    Revert "filtro das doações"
    This reverts commit 3ef288a09fa2ea103994f98bbfe4952c5c2ac5bd.

---
+++ server.js
@@ -0,0 +1,34 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+
+const rules = auth.rewriter({
+  users: 640,
+  ongs: 664,
+  donates:660
+});
+
+app.use(cors());
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port);
+
+console.log("Server is running on port:", port);
+
+/*usuario testes
+{
+  "email": "admin@mail.com",
+  "password": "admin123"
+}
+{
+  "email": "ongteste@mail.com",
+  "password": "ong123"
+}
+   */
